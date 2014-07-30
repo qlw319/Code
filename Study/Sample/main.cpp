29db734df093e67dd33f3248dea4d880ee9d3c32
qlw319 <1529899901@qq.com>
2014-07-30 19:33:03 +0800

    add inverse and swap function.

--- Study/Sample/main.cpp
+++ Study/Sample/main.cpp
@@ -1,5 +1,26 @@
+#include <assert.h>
 #include <iostream>
 using namespace std;
+
+template<typename _DataType> void swap(_DataType& v1, _DataType& v2)
+{
+	_DataType t = v1;
+	v1 = v2;
+	v2 = t;
+}
+
+template<typename _DataType> void inverse(_DataType pBegin, _DataType pEnd)
+{
+	assert(pBegin != NULL);
+	assert(pEnd != NULL);
+	assert(pBegin <= pEnd);
+	while(pBegin <= pEnd)
+	{
+		swap(*pBegin, *pEnd);
+		++pBegin;
+		--pEnd;
+	}
+}
 
 int main()
 {
